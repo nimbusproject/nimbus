17b0f746e0ce9f62f356726d9bfdcc8ce22c5936
David LaBissoniere <labisso@uchicago.edu>
2009-09-30 12:40:59 -0500

    Fixed elastic interface userdata handling Copied fix from gateway svn repo

--- messaging/gt4.0-elastic/java/msgbridge/src/org/nimbustools/messaging/gt4_0_elastic/v2008_05_05/rm/defaults/DefaultRun.java
+++ messaging/gt4.0-elastic/java/msgbridge/src/org/nimbustools/messaging/gt4_0_elastic/v2008_05_05/rm/defaults/DefaultRun.java
@@ -197,7 +197,7 @@ public class DefaultRun implements Run {
                             "be base64 encoded?");
                 }
                 final byte[] bytes = Base64.decode(base64Encoded.getBytes());
-                userData = bytes.toString();
+                userData = new String(bytes);
             }
         }
 
