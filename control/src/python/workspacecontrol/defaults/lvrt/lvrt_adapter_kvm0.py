48a351bc1970a8a478b63e1d6b7135e8e7737b80
Pierre Riteau <Pierre.Riteau@irisa.fr>
2010-01-09 01:23:34 +0800

    Use kvm instead of qemu as the libvirt domain type
    Otherwise libvirt runs the QEMU emulator, without hardware acceleration.

--- control/src/python/workspacecontrol/defaults/lvrt/lvrt_adapter_kvm0.py
+++ control/src/python/workspacecontrol/defaults/lvrt/lvrt_adapter_kvm0.py
@@ -22,6 +22,6 @@ class intakeadapter(PlatformInputAdapter):
         PlatformInputAdapter.__init__(self, params, common)
         
     def fill_model(self, dom, local_file_set, nic_set, kernel):
-        dom._type = "qemu"
+        dom._type = "kvm"
         dom.os.type = "hvm"
         
