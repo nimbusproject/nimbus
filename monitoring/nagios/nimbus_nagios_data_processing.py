40928cebd0a707e13e3773354abbfc221d698686
Adam Bishop <ahbishop@uvic.ca>
2010-02-01 10:16:02 -0800

    A newly overhauled version of nimbus_nagios_data_processing to use the new XML schema

--- monitoring/nagios/nimbus_nagios_data_processing.py
+++ monitoring/nagios/nimbus_nagios_data_processing.py
@@ -1,116 +1,57 @@
-#!/usr/bin/python -u
+#!/usr/bin/python
 
-"""*
- * Copyright 2009 University of Victoria
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND,
- * either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- *
- * AUTHOR - Adam Bishop - ahbishop@uvic.ca
- * 
- * For comments or questions please contact the above e-mail address 
- * or Ian Gable - igable@uvic.ca
- *
- * """
-
-
-__VERSION__ = '0.01'
-
-import sys
-import commands
-import os
-import logging
 from cStringIO import StringIO
-import time
-from optparse import OptionParser
-from xml.sax import make_parser
-from xml.sax.handler import ContentHandler
 import xml
+import xml.dom.pulldom
+from xml.dom.pulldom import parseString, parse, START_ELEMENT
+import sys
+import logging
+import libxml2
+import libxslt
+import time
+import os
 
-class PluginObject:
-
-        def __init__(self, callingClass):
-                self.logString = StringIO()
-
-                self.logger = logging.getLogger(callingClass)
-                self.logger.setLevel(logging.INFO)
-                formatter = logging.Formatter('%(asctime)s ; %(name)s ; %(levelname)s ; %(message)s')
-                xmlOutputHndlr = logging.StreamHandler(self.logString)
-                xmlOutputHndlr.setFormatter(formatter)
-                xmlOutputHndlr.setLevel(logging.INFO)
-
-                errorOutputHndlr = logging.StreamHandler(sys.stdout)
-                errorOutputHndlr.setFormatter(formatter)
-                errorOutputHndlr.setLevel(logging.ERROR)
-
-                self.logger.addHandler(xmlOutputHndlr)
-                self.logger.addHandler(errorOutputHndlr)
-
-
-
+TARGET_XML_FILE = "xmlCapture.xml"#"/tmp/mdsresource.xml"
+DUPLICATE_REM_XSL = "dp2.xsl"
+OUTPUT_XML_FILE = "outputXML.xml"
+TIME_WINDOW = 300
 
 PERFORMANCE_DATA_LOC = "/tmp/service-perfdata"
 TARGET_XML_FILE = "/tmp/mdsresource.xml"
 
-# This value specifies 300 seconds, or 5 minutes for out "sliding window" view into the 
-# performance data. Change this value accordingly to fit the rest of the Nagios reporting
-# intervals, but don't make it too large or "old" data will start creeping into the MDS, 
-# which may not accurately describe the cluster state
-TIME_WINDOW = 300
+class PluginObject:
 
-class NagiosPerfDataProcessor(PluginObject):
-    """ This class takes all the various snippets of XML from the Nagios service-performance-file
-    and aggregates them into a single, well formed XML document that can be utilized by our
-    Nimbus MDS Aggregator
-    """
+    def __init__(self, callingClass):
+
+        self.logger = logging.getLogger(callingClass)
+        self.logger.setLevel(logging.INFO)
+        formatter = logging.Formatter('%(asctime)s ; %(name)s ; %(levelname)s ; %(message)s')
+
+        errorOutputHndlr = logging.StreamHandler(sys.stdout)
+        errorOutputHndlr.setFormatter(formatter)
+        errorOutputHndlr.setLevel(logging.ERROR)
+
+        self.logger.addHandler(errorOutputHndlr)
+
+class NagiosXMLAggregator(PluginObject):
+    
     def __init__(self):
         PluginObject.__init__(self,self.__class__.__name__)
-        self.parser = make_parser()
-        self.curHandler = ResourceHandler()
-        self.parsedXML = ""
-        self.totalResources = []
-        self.parser.setContentHandler(self.curHandler)
+        self.xmlFile = None
+        self.readXML = None
 
-    def output(self, outputFile):
-        
-        try:
-            fileHandle = open(outputFile, "w")
 
-        except IOError:
-            self.logger.error("Unable to open \'"+outputFile+"\' for writing!")
-            sys.exit(-1)
-        
-        fileHandle.write(self.parsedXML.getvalue())
+    def aggregateServiceDataToXML(self):
 
-        fileHandle.close()
-
-    def parse(self):
-
-        finalXML = StringIO()
-        finalXML.write("<?xml version=\"1.0\" encoding=\"UTF-8\"?>")
-        finalXML.write("<ROOT>")
-        fileHandle = None
-        if not (os.path.exists(PERFORMANCE_DATA_LOC)):
+        retXML = StringIO()
+        retXML.write("<WRAPPER>")
+        if  (os.path.exists(PERFORMANCE_DATA_LOC)):
             try:
-                print "THere's no file, making an empty one"
-                fileHandle = open(PERFORMANCE_DATA_LOC,"w")
-                fileHandle.close()    
-            except IOError:
-                self.logger.error("Unable to create empty file: "+PERFORMANCE_DATA_LOC)
+                fileHandle = open(PERFORMANCE_DATA_LOC,"r")
+              
+            except IOError, err:
+                self.logger.error("IOError encountered opening "+PERFORMANCE_DATA_LOC+" for reading")
 
-        try:
-
-            fileHandle = open(PERFORMANCE_DATA_LOC,"r")
             for line in fileHandle.readlines():
                 
                 lineEntries = line.split()
@@ -119,79 +60,95 @@ class NagiosPerfDataProcessor(PluginObject):
                 
                 delta = abs(serviceTime - systemTime)
                 if(delta > TIME_WINDOW):
+                    #print "Outside TIME WINDOW" 
                     continue
                 # Ignore lines that don't contain the xml header that
                 # our client plugins include as part of the transmission
                 xmlHeaderIndex = line.find("<?xml")
                 if (xmlHeaderIndex == -1):
                     continue
-                else:
+                else: 
                     # This will only print the XML string from the found line
                     # To find the 'end' of the xml header and effectively 
                     # strip it off so the XML can be aggregated into 1 source
                     tagIndex = line.find("?>") + 2
                     resourceXMLEntry = line[tagIndex:]
-                    finalXML.write(resourceXMLEntry)
+                    retXML.write(resourceXMLEntry.strip())
+            retXML.write("</WRAPPER>") 
+            return retXML.getvalue().strip()
 
-        except IOError:
-            self.logger.error("Unable to open \'"+PERFORMANCE_DATA_LOC +"\' for reading!")
+    def outputXMLToFile(self, xmlToOutput):
+
+        try:
+
+            fileHandle = open(TARGET_XML_FILE,"w")
+            fileHandle.write(xmlToOutput)
+            fileHandle.close()
+        except IOError, err:
+            self.logger.error("IOError thrown trying to output XML to: "+TARGET_XML_FILE+" - "+str(err))
             sys.exit(-1)
 
-        fileHandle.close()
+    def removeDuplicateIPs(self, xmlToProcess):
+
+        try:
+            styledoc = libxml2.parseFile(DUPLICATE_REM_XSL)
+        except libxml2.parserError, err:
+            self.logger.error("Unable to parse XSLT: "+DUPLICATE_REM_XSL+" "+str(err))
+            sys.exit(-1)
+
+        style = libxslt.parseStylesheetDoc(styledoc)
+        try:
+            doc = libxml2.parseDoc(xmlToProcess)
+        except libxml2.parserError, err:
+            self.logger.error("Unable to parse desired XML in removeDuplcateIPs: "+str(err))
+            sys.exit(-1)
+
+        result = style.applyStylesheet(doc, None)
+        #style.saveResultToFilename(OUTPUT_XML_FILE, result, 0)
+        retXML = style.saveResultToString(result)
         
-        finalXML.write("</ROOT>")
-        self.parsedXML=finalXML
-        xml.sax.parseString(finalXML.getvalue(), self.curHandler)
-        self.totalResources = self.curHandler.getResources()
-        return self.totalResources
-
-
-
-class ResourceHandler(ContentHandler):
-    """
-    This class implements the SAX API functions 'startElement', 'endElement' and 'characters'
-    It is also intimately tied to the XML format used by the client side plugins
-    """  
-    def __init__(self): 
-         
-        self.isResource = False
-        self.isEntry = False
-        self.collectedResources = {}
-        self.repeatedResource = False
-    def startElement(self,name,attr):
-
-        if name == 'RES':
-            self.topLevelKey = attr.getValue('LOC')
-            self.secondLevelKey = attr.getValue('TYPE')
-                
-            if(self.topLevelKey not in self.collectedResources.keys()):
-                self.collectedResources[self.topLevelKey] = {}
-            if(self.secondLevelKey not in self.collectedResources[self.topLevelKey].keys()):
-                self.collectedResources[self.topLevelKey][self.secondLevelKey] = {}
-            self.isResource = True
-        elif name == 'ENTRY':
-            self.isEntry = True
-            self.thirdLevelKey = attr.getValue('ID')
-            if(self.thirdLevelKey in self.collectedResources[self.topLevelKey][self.secondLevelKey].keys()):
-                self.repeatedResource = True
-    
-    def characters (self, ch):
-        if (self.isEntry == True and self.repeatedResource == False):
-            self.collectedResources[self.topLevelKey][self.secondLevelKey][self.thirdLevelKey] = ch
-    
-    def endElement(self, name):
-        if name == 'RES':
-            self.isResource = False
-        elif name == 'ENTRY':
-            self.isEntry = False
-            self.repeatedResource = False
-    def getResources(self):
-        return self.collectedResources
+        style.freeStylesheet()
+        doc.freeDoc()
+        result.freeDoc()
         
+        return str(retXML.strip())
+
+    def __call__(self):
+
+        self.readXML = self.aggregateServiceDataToXML()
+        #print self.readXML
+        doc = parseString(self.readXML)
+
+        finalXML = StringIO()
+        finalXML.write("<?xml version=\"1.0\" encoding=\"UTF-8\"?>")
+        finalXML.write("<Cluster>")
+        finalXML.write("<HeadNode>")
+
+        for event, node in doc:
+            if event == xml.dom.pulldom.START_ELEMENT and node.localName == "HeadNode":
+                doc.expandNode(node)
+                tempString = node.toxml()
+
+                finalXML.write(tempString[10:-11])
+        finalXML.write("</HeadNode>")
+        finalXML.write("<WorkerNodes>")
+        doc = parseString(self.readXML)
+        for event, node in doc:
+            if event == xml.dom.pulldom.START_ELEMENT and node.localName =="WorkerNode":
+                doc.expandNode(node)
+                tempString = node.toxml()
+                finalXML.write(tempString) 
+        finalXML.write("</WorkerNodes>")
+        finalXML.write("</Cluster>")
+
+        return self.removeDuplicateIPs(finalXML.getvalue())
+
+
 if __name__ == '__main__':
+    
+    xmlAggr = NagiosXMLAggregator()
+    xml = xmlAggr()
+    #print xml
+    xmlAggr.outputXMLToFile(xml)
 
-    myProc = NagiosPerfDataProcessor()
-    myProc.parse()
-    myProc.output(TARGET_XML_FILE)
-
-    sys.exit(0)
+    sys.exit(0)    
