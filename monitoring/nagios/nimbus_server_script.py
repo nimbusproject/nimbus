94ad4fb36aa1867c9a8783ed92c2ed618b4f853f
Adam Bishop <ahbishop@uvic.ca>
2010-02-05 19:30:23 -0800

    Copywrite Message & Apache License

--- monitoring/nagios/nimbus_server_script.py
+++ monitoring/nagios/nimbus_server_script.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python
 
 """*
- * Copyright 2009 University of Victoria
+ * Copyright 2010 University of Victoria
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
