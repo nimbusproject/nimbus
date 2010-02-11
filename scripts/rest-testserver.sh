0c7b9512dc2e281d0b4818e5a7f0e239d952b109
David LaBissoniere <labisso@uchicago.edu>
2010-02-11 14:01:54 -0600

    Make REST testserver actually work in a clean env.

--- scripts/rest-testserver.sh
+++ scripts/rest-testserver.sh
@@ -1,7 +1,11 @@
 #!/bin/sh
 
+NIMBUS_PRINTNAME="Run Nimbus REST test server"
+NIMBUS_ANT_CMD="run-rest-testserver $*"
+
 BASEDIR_REL="`dirname $0`/.."
 BASEDIR=`cd $BASEDIR_REL; pwd`
+RUN=$BASEDIR/scripts/lib/gt4.0/build/run.sh
 
-cd "$BASEDIR/messaging/rest/java/source/"
-ant testserver
+export NIMBUS_PRINTNAME NIMBUS_ANT_CMD
+exec sh $RUN
