4018659110adb17017f070aca4a23fbb857274e6
Alex Clemesha <clemesha@ucsd.edu>
2010-02-11 18:23:44 -0800

    Test for succesful access and secret key creation after User creation.

--- web/src/python/nimbusrest/tests.py
+++ web/src/python/nimbusrest/tests.py
@@ -15,9 +15,13 @@ class TestNimbusRestClient(unittest.TestCase):
         self.secret = "secret"
         self.conn = AdminConnection(self.uri, self.key, self.secret)
 
-    def test_add_user(self):
+    def test_add_user_has_key_secret(self):
         resp = self.conn.add_user(self.user) 
         self.assertEquals(resp.dn, "test_dn")
+        user_id = resp.user_id
+        access_key = self.conn.generate_user_access_key(user_id)
+        self.assertTrue(hasattr(access_key, "secret"))
+        self.assertEquals(access_key.key, user_id)
 
 
 if __name__ == '__main__':
