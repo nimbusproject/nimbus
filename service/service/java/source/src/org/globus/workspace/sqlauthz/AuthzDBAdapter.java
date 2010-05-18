9080d3268487271d4f2a1704e45c992d2994b17d
BuzzTroll <bresnaha@mcs.anl.gov>
2010-05-18 10:37:00 -1000

    adding cumulus autorization modules to the system

---
+++ service/service/java/source/src/org/globus/workspace/sqlauthz/AuthzDBAdapter.java
@@ -0,0 +1,331 @@
+package org.globus.workspace.sqlauthz;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.globus.workspace.Lager;
+import org.globus.workspace.persistence.WorkspaceDatabaseException;
+
+import javax.sql.DataSource;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+/**
+ * Created by John Bresnahan
+ * User: bresnaha
+ * Date: May 17, 2010
+ * Time: 9:38:44 AM
+ * <p/>
+ * org.globus.workspace.sqlauthz
+ */
+public class AuthzDBAdapter
+{
+    private static final String FIND_PARENT_OBJECT_BY_NAME = "Select id from objects where name = ? and parent_id is NULL and object_type = ?";
+    private static final String FIND_OBJECT_BY_NAME = "Select id from objects where name = ? and parent_id = ?";
+    private static final String FIND_USER_BY_ALIAS = "Select user_id from user_alias where alias_name = ? and alias_type = ?";
+    private static final String CHECK_PERMISSIONS = "Select access_type_id from object_acl where object_id = ? and user_id = ?";
+    private static final String GET_DATA_KEY = "select data_key from objects where id = ?";
+
+    public static final int ALIAS_TYPE_S3 = 1;
+    public static final int ALIAS_TYPE_DN = 2;
+    
+    public static final int OBJECT_TYPE_S3 = 1;
+
+
+    private final DataSource            dataSource;
+
+    private static final Log logger =
+            LogFactory.getLog(AuthzDBAdapter.class.getName());
+
+    public AuthzDBAdapter(
+        DataSource                      dataSourceImpl)
+    {
+        if (dataSourceImpl == null)
+        {
+            throw new IllegalArgumentException("dataSourceImpl may not be null");
+        }
+        this.dataSource = dataSourceImpl;
+    }
+
+    public String getCanonicalUserIdFromS3(
+        String                          name)
+            throws   WorkspaceDatabaseException
+    {
+          return getCanonicalUserIdFromAlias(name, ALIAS_TYPE_S3);
+    }
+
+    public String getCanonicalUserIdFromDn(
+        String                          name)
+            throws   WorkspaceDatabaseException
+    {
+          return getCanonicalUserIdFromAlias(name, ALIAS_TYPE_DN);
+    }
+
+    public int getParentObject(
+        String                          objectName)
+            throws WorkspaceDatabaseException
+    {
+        Connection c = null;
+        PreparedStatement pstmt = null;
+
+        try
+        {
+            c = getConnection();
+            pstmt = c.prepareStatement(FIND_PARENT_OBJECT_BY_NAME);
+            pstmt.setString(1, objectName);
+            ResultSet rs = pstmt.executeQuery();
+            if(!rs.next())
+            {
+                throw new WorkspaceDatabaseException("now such parent file found  " + objectName);
+            }
+            int objectId = rs.getInt(1);
+            return objectId;
+        }
+        catch(SQLException e)
+        {
+            logger.error("",e);
+            throw new WorkspaceDatabaseException(e);
+        }
+        finally
+        {
+            try
+            {
+                if (pstmt != null)
+                {
+                    pstmt.close();
+                }
+                if (c != null)
+                {
+                    returnConnection(c);
+                }
+            }
+            catch (SQLException sql)
+            {
+                logger.error("SQLException in finally cleanup", sql);
+            }
+        }
+    }
+
+    public String getDataKey(
+        int                             objectId)
+            throws WorkspaceDatabaseException
+    {
+        Connection c = null;
+        PreparedStatement pstmt = null;
+
+        try
+        {
+            c = getConnection();
+            pstmt = c.prepareStatement(GET_DATA_KEY);
+            pstmt.setInt(1, objectId);
+            ResultSet rs = pstmt.executeQuery();
+            if(!rs.next())
+            {
+                throw new WorkspaceDatabaseException("now such file id found  " + objectId);
+            }
+            String dataKey = rs.getString(1);
+            return dataKey;
+        }
+        catch(SQLException e)
+        {
+            logger.error("",e);
+            throw new WorkspaceDatabaseException(e);
+        }
+        finally
+        {
+            try
+            {
+                if (pstmt != null)
+                {
+                    pstmt.close();
+                }
+                if (c != null)
+                {
+                    returnConnection(c);
+                }
+            }
+            catch (SQLException sql)
+            {
+                logger.error("SQLException in finally cleanup", sql);
+            }
+        }
+    }
+
+    public int getFileID(
+        String                          parentName,
+        String                          fileName,
+        int                             objectType)
+            throws WorkspaceDatabaseException
+    {
+        if(parentName == null)
+        {
+            return getParentObject(fileName);
+        }
+
+        Connection c = null;
+        PreparedStatement pstmt = null;
+
+        int parentId = getParentObject(parentName);
+        try
+        {
+            c = getConnection();
+            pstmt = c.prepareStatement(FIND_OBJECT_BY_NAME);
+            pstmt.setString(1, fileName);
+            pstmt.setInt(1, parentId);
+            pstmt.setInt(1, objectType);
+            ResultSet rs = pstmt.executeQuery();
+            if(!rs.next())
+            {
+                throw new WorkspaceDatabaseException("now such file found  " + fileName);
+            }
+            int objectId = rs.getInt(1);
+            return objectId;
+        }
+        catch(SQLException e)
+        {
+            logger.error("",e);
+            throw new WorkspaceDatabaseException(e);
+        }
+        finally
+        {
+            try
+            {
+                if (pstmt != null)
+                {
+                    pstmt.close();
+                }
+                if (c != null)
+                {
+                    returnConnection(c);
+                }
+            }
+            catch (SQLException sql)
+            {
+                logger.error("SQLException in finally cleanup", sql);
+            }
+        }
+
+    }
+    
+    public String getPermissions(
+        int                             objectId,
+        String                          userId)
+            throws WorkspaceDatabaseException
+    {
+        Connection c = null;
+        PreparedStatement pstmt = null;
+
+        try
+        {
+            c = getConnection();
+            pstmt = c.prepareStatement(CHECK_PERMISSIONS);
+            pstmt.setInt(1, objectId);
+            pstmt.setString(1, userId);
+            ResultSet rs = pstmt.executeQuery();
+
+            String perms = "";
+            while(rs.next())
+            {
+                String ch = rs.getString(1);
+                perms = perms + ch;
+            }
+            return perms;
+        }
+        catch(SQLException e)
+        {
+            logger.error("",e);
+            throw new WorkspaceDatabaseException(e);
+        }
+        finally
+        {
+            try
+            {
+                if (pstmt != null)
+                {
+                    pstmt.close();
+                }
+                if (c != null)
+                {
+                    returnConnection(c);
+                }
+            }
+            catch (SQLException sql)
+            {
+                logger.error("SQLException in finally cleanup", sql);
+            }
+        }
+    }
+
+
+    public String getCanonicalUserIdFromAlias(
+        String                          name,
+        int                             type)
+            throws   WorkspaceDatabaseException
+    {
+        Connection c = null;
+        PreparedStatement pstmt = null;
+
+        try
+        {
+            c = getConnection();
+            pstmt = c.prepareStatement(FIND_USER_BY_ALIAS);
+            pstmt.setString(1, name);
+            pstmt.setInt(2, type);
+            logger.debug("getting user " + pstmt.toString());
+            ResultSet rs = pstmt.executeQuery();
+
+            if(!rs.next())
+            {                
+                throw new WorkspaceDatabaseException("no such user found  " + name);
+            }
+            String canUserId = rs.getString(1);
+
+            return canUserId;
+
+        }
+        catch(SQLException e)
+        {
+            logger.error("",e);
+            throw new WorkspaceDatabaseException(e);
+        }
+        finally
+        {
+            try
+            {
+                if (pstmt != null)
+                {
+                    pstmt.close();
+                }
+                if (c != null)
+                {
+                    returnConnection(c);
+                }
+            }
+            catch (SQLException sql)
+            {
+                logger.error("SQLException in finally cleanup", sql);
+            }
+        }
+    }
+
+    private Connection getConnection() throws SQLException
+    {
+        return this.dataSource.getConnection();
+    }
+
+    private void returnConnection(Connection connection)
+    {
+        if(connection != null)
+        {
+            try
+            {
+                connection.close();
+            }
+            catch(SQLException e)
+            {
+                logger.error("",e);
+            }
+        }
+    }
+}
