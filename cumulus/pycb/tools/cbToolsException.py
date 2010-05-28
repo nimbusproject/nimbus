11327d7a39e613e084acbd6bdb91ec744da52bdf
BuzzTroll <bresnaha@mcs.anl.gov>
2010-05-28 13:47:49 -1000

    making sure the other files get added too.
    still need tests for the new command line programs

---
+++ cumulus/pycb/tools/cbToolsException.py
@@ -0,0 +1,41 @@
+import traceback
+import sys
+import uuid
+import pycb
+import logging
+from xml.dom.minidom import Document
+
+class cbToolsException(Exception):
+
+    errors = {}
+    rcs = {}
+
+    errors['UNKNOWN_USER'] = "The user %s is unknown"
+    rcs['UNKNOWN_USER'] = 1
+    errors['CMDLINE'] = "poorly formed command line"
+    rcs['CMDLINE'] = 2
+
+    def __init__(self, code, fmt_set, ex=None):
+        self.code = code 
+        self.ex = ex
+        self.msg = cbToolsException.errors[self.code] % fmt_set
+        self.rc = cbToolsException.rcs[self.code]
+
+    def get_code(self):
+        return self.code
+
+    def get_msg(self):
+        return self.msg
+
+    def get_exception(self):
+        return self.ex
+
+    def get_rc(self):
+        return self.rc
+
+    def __str__(self):
+        msg = self.msg
+        if self.ex != None:
+            msg = msg + " : " + str(self.ex)
+        return msg
+
---
+++ cumulus/pycb/tools/list_users.py
@@ -0,0 +1,74 @@
+#!/usr/bin/python
+import getpass
+import traceback
+import string
+import random
+import os
+import sys
+from ConfigParser import SafeConfigParser
+import pycb
+import pynimbusauthz
+import shutil
+import pycb.cbPosixSecurity
+from pycb.tools import cbToolsException
+from pynimbusauthz.cmd_opts import cbOpts
+
+g_report_options = ["friendly", "ID", "password", "quota", "canonical_id"]
+
+class printer_obj(object):
+    def __init__(self):
+        self.x = 1
+
+def setup_options(argv):
+
+    u = """[options] [<display name pattern>]"""
+    (parser, all_opts) = pynimbusauthz.get_default_options(u)
+
+    opt = cbOpts("report", "r", "Report the selected columns from the following: " + pycb.tools.report_options_to_string(g_report_options), pycb.tools.report_options_to_string(g_report_options))
+    all_opts.append(opt)
+    opt = cbOpts("delim", "d", "The column separater for the report.", ",")
+    all_opts.append(opt)
+
+    (o, args) = pynimbusauthz.parse_args(parser, all_opts, argv)
+
+    return (o, args)
+
+def main(argv=sys.argv[1:]):
+
+    auth = pycb.config.auth
+
+    (opts, args) = setup_options(argv)
+    if len(args) == 0:
+        raise cbToolsException('CMDLINE', ["You must provide a display name"])
+
+    search_name = args[0].replace("*", "%%")
+
+    all_matches = auth.find_user_id_by_display(search_name)
+
+    for id in all_matches:
+        u = auth.get_user(id)
+        p = printer_obj() 
+        p.ID = id
+        p.canonical_id = u.get_canonical_id()
+        p.quota = u.get_quota()
+        p.password = u.get_password()
+        p.friendly = u.get_display_name()
+
+        pycb.tools.print_report(p, opts.report, opts)
+
+    return 0
+
+if __name__ == "__main__":
+    try:
+        rc = main()
+    except cbToolsException, tex:
+        print tex
+        rc = tex.get_rc()
+    except SystemExit:
+        rc = 127
+    except:
+        traceback.print_exc(file=sys.stdout)
+        print 'An unknown error occurred'
+        rc = 128
+    sys.exit(rc)
+
