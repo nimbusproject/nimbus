929da7ce29583e58de8a26708bfeb43a79fb198e
BuzzTroll <bresnaha@mcs.anl.gov>
2010-06-02 12:50:09 -1000

    added some error handling patches and tests
    files should not leak disk space on error now.

---
+++ cumulus/tests/s3cmd/test_connection_drop.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+bucket_name=CumulusTest$RANDOM
+fname=GRP$RANDOM
+s3cmd mb s3://$bucket_name
+
+# make a big enough file
+f=`mktemp`
+dd if=/dev/zero of=$f count=1024 bs=102400
+
+s3cmd put $f s3://$bucket_name/$fname &
+pid=$!
+kill $pid
+rm $f
+
+#s3cmd info s3://$bucket_name/$fname
+#if [ "X$?" == "X0" ]; then
+#    echo "ERROR: the file should not be there"
+#    exit 1
+#fi
+s3cmd del s3://$bucket_name/$fname
+if [ "X$?" == "X0" ]; then
+    echo "ERROR: the file should not be there"
+    exit 1
+fi
+
+# cleanup
+s3cmd rb s3://$bucket_name
+if [ "X$?" != "X0" ]; then
+    echo "ERROR: delete bucket failed"
+    exit 1
+fi
+
+exit 0
