6b4aecf389ab8e9faac9ce2b08a6dac2616afbdc
Paulo Ricardo Motta Gomes <pauloricardomg@gmail.com>
2010-06-14 13:23:04 +0200

    added vm id tracking in SIRequest

--- service/service/java/source/src/org/globus/workspace/spotinstances/SIRequest.java
+++ service/service/java/source/src/org/globus/workspace/spotinstances/SIRequest.java
@@ -1,17 +1,19 @@
 package org.globus.workspace.spotinstances;
 
+import java.util.LinkedHashSet;
+import java.util.LinkedList;
+
 import org.globus.workspace.service.binding.vm.VirtualMachine;
 import org.nimbustools.api.repr.Caller;
 import org.nimbustools.api.repr.ctx.Context;
 import org.nimbustools.api.repr.vm.NIC;
+
 
 public class SIRequest implements Comparable<SIRequest>{
  
     private String id;
     private Double maxBid;
     private Integer requestedInstances;
-    private Integer allocatedInstances;
-    private Integer fulfilledInstances;
     private boolean persistent;
     private SIRequestStatus status;
     
@@ -20,6 +22,9 @@ public class SIRequest implements Comparable<SIRequest>{
     private Context context;
     private NIC[] requestedNics;
     private String groupID;
+    
+    private LinkedHashSet<Integer> allocatedVMs = new LinkedHashSet<Integer>();
+    private LinkedHashSet<Integer> fulfilledVMs = new LinkedHashSet<Integer>();
 
     public SIRequest(String id, Double highestPrice, Integer requestedInstances) {
         this(id, highestPrice, requestedInstances, false);
@@ -30,8 +35,6 @@ public class SIRequest implements Comparable<SIRequest>{
         this.maxBid = highestPrice;
         this.requestedInstances = requestedInstances;
         this.persistent = persistent;
-        this.allocatedInstances = 0;
-        this.fulfilledInstances = 0;
         this.status = SIRequestStatus.OPEN;
     }  
 
@@ -39,8 +42,6 @@ public class SIRequest implements Comparable<SIRequest>{
             Caller caller, String groupID, VirtualMachine[] bindings, Context context,
             NIC[] requestedNics) {
         this.requestedInstances = bindings.length;
-        this.allocatedInstances = 0;
-        this.fulfilledInstances = 0;
         this.status = SIRequestStatus.OPEN;        
         this.id = id;
         this.maxBid = spotPrice;
@@ -59,32 +60,33 @@ public class SIRequest implements Comparable<SIRequest>{
         this.maxBid = maxBid;
     }
     
-    public Integer getRequestedInstances(){
-        return this.requestedInstances;
-    }    
-    
     public Integer getNeededInstances(){
         if(this.persistent){
             return this.requestedInstances;
         } else {
-            return this.requestedInstances-this.fulfilledInstances;
+            return this.requestedInstances-this.fulfilledVMs.size();
         }
     }
     
     public Integer getAllocatedInstances() {
-        return allocatedInstances;
+        return allocatedVMs.size();
     }
 
-    public void setAllocatedInstances(Integer allocatedInstances) {
-        this.allocatedInstances = allocatedInstances;
+    public void addCreatedVMs(int[] createdIds) {
+        if(createdIds != null && createdIds.length > 0){
+            for (int i = 0; i < createdIds.length; i++) {
+                allocatedVMs.add(createdIds[i]);
+            }
+            this.setStatus(SIRequestStatus.ACTIVE);
+        }
     }
     
-    public Integer getPendingInstances(){
-        return this.getNeededInstances() - this.allocatedInstances;
+    public Integer getUnallocatedInstances(){
+        return this.getNeededInstances() - getAllocatedInstances();
     }
     
     public Boolean needsMoreInstances(){
-        return !this.getPendingInstances().equals(0);
+        return !this.getUnallocatedInstances().equals(0);
     }
 
     public String getId() {
@@ -103,16 +105,8 @@ public class SIRequest implements Comparable<SIRequest>{
         return status;
     }
 
-    public void setStatus(SIRequestStatus status) {
+    private void setStatus(SIRequestStatus status) {
         this.status = status;
-    }
-    
-    public void addFulfilledInstances(Integer quantity){
-        if((this.fulfilledInstances+quantity) >= this.requestedInstances){
-            this.fulfilledInstances = requestedInstances;
-        } else {
-            fulfilledInstances += quantity;
-        }  
     }
 
     @Override
@@ -138,15 +132,90 @@ public class SIRequest implements Comparable<SIRequest>{
 
     public String getGroupID() {
         return groupID;
-    }    
-
+    }
+    
+    public boolean isAllocatedVM(Integer vmid){        
+        return allocatedVMs.contains(vmid);
+    }
+    
     @Override
     public String toString() {
         return "SIRequest [id " + id + ", status= " + status + ", requestedInstances="
-                + requestedInstances + ", allocatedInstances=" + allocatedInstances
+                + requestedInstances + ", allocatedInstances=" + getAllocatedInstances()
                 + ", maxBid=" + maxBid + ", persistent=" + persistent 
                 + ", caller=" + caller + "]";
     }
+
+    public void fulfillVM(int vmid, Double spotPrice) {
+        if(this.allocatedVMs.remove(vmid)){
+            if(this.persistent){
+                //If the request is persistent, it is going to be considered again
+                //for fulfillment, so, clone VirtualMachine and unset ID to avoid 
+                //garbage from previous object
+                for (int i = 0; i < bindings.length; i++) {
+                    VirtualMachine vm = bindings[i];
+                    if(vm.getID() == vmid){
+                        try {
+                            //TODO: Check if this procedure is OK
+                            bindings[i] = VirtualMachine.cloneOne(vm);
+                            bindings[i].setID(-1);
+                        } catch (Exception e) {
+                            // won't happen
+                        }
+                        break;
+                    }
+                }
+            }
+            
+            fulfilledVMs.add(vmid);
+            changeStatus(spotPrice);
+        }
+    }
+
+    private void changeStatus(Double spotPrice) {
+        if(this.allocatedVMs.isEmpty()){
+            if(!this.persistent && (!needsMoreInstances() || spotPrice > this.maxBid)){
+                this.setStatus(SIRequestStatus.CLOSED);
+            } else {
+                this.setStatus(SIRequestStatus.OPEN);
+            }
+        }
+    }
     
+    public VirtualMachine[] getUnallocatedVMs(int quantity) throws SIRequestException{
+        if(this.getUnallocatedInstances() < quantity){
+            throw new SIRequestException("Requested " + quantity + " unallocated VMs, but there are only " + this.getUnallocatedInstances() + ".");
+        }        
+        
+        VirtualMachine[] result = new VirtualMachine[quantity];
+        int j = 0;
+        
+        for (int i = 0; i < quantity; i++) {
+            VirtualMachine current = bindings[i];
+            if(!current.getID().equals(-1)){
+                result[j++] = current;
+            }
+        }
+        
+        return result;
+    }
     
+    public int[] getAllocatedVMs(int quantity) throws SIRequestException{
+        if(this.getAllocatedInstances() < quantity){
+            throw new SIRequestException("Requested " + quantity + " allocated VMs, but there are only " + getAllocatedInstances() + ".");
+        }
+        
+        int[] result = new int[quantity];
+                
+        LinkedList<Integer> allocations = new LinkedList<Integer>(allocatedVMs);
+        for (int i = 0; i < quantity; i++) {
+            if(!allocations.isEmpty()){
+                //Since this will be used for pre-emption
+                //getting more recent allocations (last on the list)
+                result[i] = allocations.removeLast();
+            }
+        }
+        
+        return result;
+    }    
 }
---
+++ service/service/java/source/src/org/globus/workspace/spotinstances/SIRequestException.java
@@ -0,0 +1,14 @@
+package org.globus.workspace.spotinstances;
+
+public class SIRequestException extends Exception {
+
+    public SIRequestException(String message) {
+        super(message);
+    }
+
+    /**
+     * 
+     */
+    private static final long serialVersionUID = 1L;
+
+}
