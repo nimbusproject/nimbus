d3d9b5829f85f232252a8794f239dbb50899a18c
BuzzTroll <bresnaha@mcs.anl.gov>
2010-06-15 09:51:47 -0500

    updating new-user tool to reflect tool changes

--- cumulus/src/bin/new-user.sh
+++ cumulus/src/bin/new-user.sh
@@ -2,24 +2,26 @@
 
 # my own tool for testing new users
 
-nh=/home/bresnaha/NIM2
+nh=/home/bresnaha/NIM
 
 cd $nh
+cd cumulus
+source env.sh
 email=$1
 dn=$2
 
-x=`./cumulus/bin/cumulus-add-user.sh -n -g "$email"`
-s3id=`echo $x | awk '{ print $7 }'`
-s3pw=`echo $x | awk '{ print $9 }'`
+$nh/cumulus/bin/cumulus-add-user "$email"
+s3id=`echo $x | awk -F , '{ print $7 }'`
+x=`$nh/cumulus/bin/cumulus-list-users -b -r ID,password,canonical_id "$email"`
 
 echo $x
+s3id=`echo $x | awk -F , '{ print $1 }'`
+s3pw=`echo $x | awk -F , '{ print $2 }'`
+canuser=`echo $x | awk -F , '{ print $3 }'`
 echo "s3 user id:  $s3id"
 echo "s3 password: $s3pw"
+echo "canonical id: $canuser"
 
-y=`./cumulus/bin/cloudfs-list-user.sh -s $s3id`
-canuser=`echo $y | awk '{ print $2 }'`
-echo "canonical user is $canuser"
-
-$nh/cumulus/bin/cloudfs-adduser.sh -a "$dn" -t x509 $canuser
-$nh/cumulus/bin/cloudfs-list-user.sh -a $canuser
+$nh/cumulus/bin/nimbusauthz-add-user -a "$dn" -t x509 $canuser
+$nh/cumulus/bin/nimbusauthz-list-users -a $canuser
 
