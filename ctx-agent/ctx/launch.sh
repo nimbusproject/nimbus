a29eebc30939bca3f05687ec16ce6c144422bc37
Tim Freeman <tim@timfreeman.org>
2010-06-19 16:41:13 -0500

    better explanation of --polltime

--- ctx-agent/ctx/launch.sh
+++ ctx-agent/ctx/launch.sh
@@ -5,14 +5,18 @@ BASEDIR=`dirname $0`
 PYTHONPATH="$BASEDIR/lib/:$BASEDIR/lib/pylib:$PYTHONPATH"
 export PYTHONPATH
 
-# This loops forever until there is a network address on one of the configured interfaces,
-# see at the top of the "wait_for_network.py" file to change the options (currently looks
-# for something on either eth0 and eth1).
+# This loops forever until there is a network address on one of the
+# configured interfaces, see at the top of the "wait_for_network.py"
+# file to change the options (currently looks for something on either
+# eth0 and eth1).
 
 python $BASEDIR/lib/wait_for_network.py 
 
 
-# The larger your cluster is going to be, the higher polltime should probably be.
+# The larger your cluster is going to be, the higher "--polltime" should
+# probably be.  The context broker will tell VMs to "come back later"
+# if all of the VM's information is not known.  This "--polltime" is the
+# delay of the agent before it goes back to re-query. 
 
 python $BASEDIR/lib/nimbus_ctx_retrieve.py -c $BASEDIR/ctx.conf --polltime 2 -z -t
 
