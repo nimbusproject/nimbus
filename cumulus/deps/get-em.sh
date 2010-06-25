031f3f525e6513d7bd7edd31cfe0bc095fc23e76
BuzzTroll <bresnaha@mcs.anl.gov>
2010-06-25 17:03:58 -0500

    iadding some good stuff and some stuff that i need to fix before a push

---
+++ cumulus/deps/get-em.sh
@@ -0,0 +1,15 @@
+#/bin/bash
+
+if [ ! -f cumulus-deps-tar.gz ]; then
+    wget http://www.mcs.anl.gov/~bresnaha/cumulus/cumulus-deps.tar.gz
+    if [ $? -ne 0 ]; then
+        echo "wget failed"
+        exit 1
+    fi
+    tar -zxvf cumulus-deps.tar.gz
+    if [ $? -ne 0 ]; then
+        echo "wget failed"
+        exit 1
+    fi
+fi
+exit 0
