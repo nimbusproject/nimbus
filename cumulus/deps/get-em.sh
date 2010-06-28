a22fe1cfa02ee85463985423992db597fc8c82c4
Tim Freeman <tim@timfreeman.org>
2010-06-27 22:13:35 -0500

    typo in get-em

--- cumulus/deps/get-em.sh
+++ cumulus/deps/get-em.sh
@@ -1,6 +1,6 @@
 #/bin/bash
 
-if [ ! -f cumulus-deps-tar.gz ]; then
+if [ ! -f cumulus-deps.tar.gz ]; then
     wget http://www.mcs.anl.gov/~bresnaha/cumulus/cumulus-deps.tar.gz
     if [ $? -ne 0 ]; then
         echo "wget failed"
