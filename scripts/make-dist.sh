ceabacbd4652cfe767a82ff2cacc00fd0b8bf776
Paulo Ricardo Motta Gomes <pauloricardomg@gmail.com>
2010-07-02 11:16:05 +0200

    fixed relative dir bug, added default dest dir and added a few output messages to make-dist

--- scripts/make-dist.sh
+++ scripts/make-dist.sh
@@ -1,21 +1,31 @@
 #!/bin/bash
 
 if ([ "X$1" == "X--help" ] || [ "X$1" == "X-h" ]); then
-    echo "make-dist.sh <directory where the resulting tarballs end up> [<git repository>]"
+    echo "Usage:"
+    echo "make-dist.sh [<destination directory>] [<git repository>]"
+    echo "Default: make-dist . git://github.com/nimbusproject/nimbus.git"
     exit 0
 fi
 
-
-dest_dir=$1
-co_dir=`mktemp -d -t tmp.XXXXXXXXXX`
+if [ "X$2" != "X" ]; then
+#   normalize dir, otherwise it is relative to $co_dir
+    dest_dir=`cd "$1";pwd`
+else
+    dest_dir=`pwd`
+fi
 
 if [ "X$2" != "X" ]; then
     repo=$2
 else
-    repo="git@github.com:nimbusproject/nimbus.git"
+    repo="git://github.com/nimbusproject/nimbus.git"
 fi 
 
-echo $co_dir
+co_dir=`mktemp -d -t tmp.XXXXXXXXXX`
+
+echo "Destination directory: $dest_dir"
+echo "Nimbus git repository: $repo"
+echo "Created temp directory: $co_dir"
+
 cd $co_dir
 git clone $repo
 
@@ -27,5 +37,7 @@ ant -f scripts/lib/gt4.0/dist/build.xml create-dist
 ls scripts/lib/gt4.0/dist/result/
 cp scripts/lib/gt4.0/dist/result/*.tar.gz $dest_dir
 
-echo $co_dir
-#rm -rf $co_dir
+echo "Removing temp dir: $co_dir"
+rm -rf $co_dir
+
+echo "Finished. Tarballs copied to $dest_dir."
