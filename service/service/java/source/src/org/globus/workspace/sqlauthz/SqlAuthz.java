377ff32715cb3a72e07016f33b1b1e37e20a042d
Tim Freeman <tim@timfreeman.org>
2010-07-12 14:37:01 -0500

    cleanup unused imports and variables

--- service/service/java/source/src/org/globus/workspace/sqlauthz/SqlAuthz.java
+++ service/service/java/source/src/org/globus/workspace/sqlauthz/SqlAuthz.java
@@ -16,28 +16,10 @@
 
 package org.globus.workspace.sqlauthz;
 
-import org.globus.workspace.groupauthz.DecisionLogic;
 import org.globus.workspace.groupauthz.GroupAuthz;
-import org.globus.workspace.persistence.WorkspaceDatabaseException;
-import org.globus.workspace.service.binding.authorization.CreationAuthorizationCallout;
-import org.globus.workspace.service.binding.authorization.Decision;
-import org.globus.workspace.service.binding.authorization.PostTaskAuthorization;
-import org.globus.workspace.service.binding.vm.VirtualMachine;
-import org.globus.workspace.service.binding.vm.VirtualMachinePartition;
-import org.nimbustools.api.services.rm.AuthorizationException;
-import org.nimbustools.api.services.rm.ResourceRequestDeniedException;
-import org.apache.commons.logging.Log;
-import org.apache.commons.logging.LogFactory;
-import java.sql.*;
-import javax.security.auth.Subject;
-import javax.sql.DataSource;
-import java.net.URI;
-import java.util.Vector;
 
 public class SqlAuthz extends GroupAuthz
 {
-    private static final Log logger =
-            LogFactory.getLog(SqlAuthz.class.getName());
     private AuthzDecisionLogic                  authD = null;
 
     public SqlAuthz(AuthzDecisionLogic authd)
