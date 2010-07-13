b30b66690c4cfc868acb00d34e1cf84c7baf56d5
David LaBissoniere <labisso@uchicago.edu>
2010-07-12 20:19:42 -0500

    Grab GT env variables from generated sourcefile.
    Removes the need to adjust trusted certs directory in two places.

--- home/libexec/run-services.sh
+++ home/libexec/run-services.sh
@@ -3,11 +3,7 @@
 NIMBUS_HOME_REL="`dirname $0`/.."
 NIMBUS_HOME=`cd $NIMBUS_HOME_REL; pwd`
 
-GLOBUS_LOCATION="$NIMBUS_HOME/services"
-export GLOBUS_LOCATION
-
-X509_CERT_DIR="$NIMBUS_HOME/var/ca/trusted-certs"
-export X509_CERT_DIR
+source "$NIMBUS_HOME/libexec/environment.sh"
 
 LOGFILE="$NIMBUS_HOME/var/services.log"
 PORT="8443"
