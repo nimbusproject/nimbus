f119ffb1edede6a88764569c59a24a8e6913a6ee
BuzzTroll <bresnaha@mcs.anl.gov>
2010-08-17 11:19:41 -0500

    2 new propagation tests.  1 that sends newname at save time.  1 that saves back to the original target name

---
+++ tests/cc-prop2-test.py
@@ -0,0 +1,56 @@
+#!/usr/bin/env python
+
+import pexpect
+import sys
+import os
+import filecmp
+
+cc_home=os.environ['CLOUD_CLIENT_HOME']
+logfile = sys.stdout
+newname="XXXX"
+try:
+    os.remove(newname)
+except:
+    pass
+cmd = "%s/bin/cloud-client.sh --transfer --sourcefile /etc/group" % (cc_home)
+(x, rc)=pexpect.run(cmd, withexitstatus=1)
+
+cmd = "%s/bin/cloud-client.sh --run --name group --hours .25" % (cc_home)
+child = pexpect.spawn (cmd, timeout=30, maxread=20000, logfile=logfile)
+rc = child.expect ('Running:')
+if rc != 0:
+    print "group not found in the list"
+    sys.exit(1)
+handle = child.readline().strip().replace("'", "")
+rc = child.expect(pexpect.EOF)
+if rc != 0:
+    print "run"
+    sys.exit(1)
+
+cmd = "%s/bin/cloud-client.sh --handle %s --save --newname %s" % (cc_home, handle, newname)
+print cmd
+(x, rc)=pexpect.run(cmd, withexitstatus=1)
+print x
+if rc != 0:
+    print "failed to save"
+    sys.exit(1)
+cmd = "%s/bin/cloud-client.sh --list" % (cc_home)
+(x, rc)=pexpect.run(cmd, withexitstatus=1)
+cmd = "%s/bin/cloud-client.sh --download --name %s --localfile %s" % (cc_home, newname, newname)
+print cmd
+(x, rc)=pexpect.run(cmd, withexitstatus=1)
+print x
+if rc != 0:
+    print "failed to terminate"
+    sys.exit(1)
+
+rc = filecmp.cmp(newname, "/etc/group")
+os.remove(newname)
+if rc:
+    sys.exit(0)
+else:
+    print "files differ"
+    sys.exit(1)
+sys.exit(0)
+
+
---
+++ tests/cc-propsamename-test.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python
+
+import pexpect
+import sys
+import os
+import filecmp
+
+cc_home=os.environ['CLOUD_CLIENT_HOME']
+logfile = sys.stdout
+newname="XXXX"
+try:
+    os.remove(newname)
+except:
+    pass
+cmd = "%s/bin/cloud-client.sh --transfer --sourcefile /etc/group" % (cc_home)
+(x, rc)=pexpect.run(cmd, withexitstatus=1)
+
+cmd = "%s/bin/cloud-client.sh --run --name group --hours .25" % (cc_home)
+child = pexpect.spawn (cmd, timeout=30, maxread=20000, logfile=logfile)
+rc = child.expect ('Running:')
+if rc != 0:
+    print "group not found in the list"
+    sys.exit(1)
+handle = child.readline().strip().replace("'", "")
+rc = child.expect(pexpect.EOF)
+if rc != 0:
+    print "run"
+    sys.exit(1)
+
+cmd = "%s/bin/cloud-client.sh --handle %s --save" % (cc_home, handle)
+print cmd
+(x, rc)=pexpect.run(cmd, withexitstatus=1)
+print x
+if rc != 0:
+    print "failed to save"
+    sys.exit(1)
+sys.exit(0)
+
+
