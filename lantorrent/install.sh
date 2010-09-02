5a8280c1adf76495c70bcf30853ed4afdb4c4d3a
BuzzTroll <bresnaha@mcs.anl.gov>
2010-09-01 21:34:32 -0500

    making the installer create the requests db

--- lantorrent/install.sh
+++ lantorrent/install.sh
@@ -1,3 +1,15 @@
 #!/bin/bash
 
+if [ "X$NIMBUS_HOME" == "X" ]; then
+    echo "NIMBUS_HOME is not set."
+    echo "This installation program is only used for the head node."
+    echo "It is not used on the VMMs and it is typically only called from the main installer"
+    exit 
+fi
+
+dir=`dirname $0`
+cd $dir
 cp -r `pwd` $NIMBUS_HOME/
+cd $NIMBUS_HOME/lantorrent
+sqlite3 etc/req.db  < etc/lt.sql
+exit $?
