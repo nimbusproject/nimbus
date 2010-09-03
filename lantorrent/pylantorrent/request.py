e3fb332f3e2a2ab747e04c16b7d6c6f2781f2332
BuzzTroll <bresnaha@mcs.anl.gov>
2010-09-03 15:53:15 -0500

    recreating work after the 'incident'

--- lantorrent/pylantorrent/request.py
+++ lantorrent/pylantorrent/request.py
@@ -12,80 +12,6 @@ import uuid
 import time
 import datetime
 
-def getrows(c, group_id):
-    s = "select hostname,port,src_filename,dst_filename,rid from requests where group_id = ? and state = 0"
-    data = (group_id, )
-    c.execute(s, data)
-    rows = c.fetchall()
-    return rows
-
-def do_it_live(con, group_id):
-
-    degree = 1
-    maxd = 3
-
-    c = con.cursor()
-    rows = getrows(c, group_id)
-    while len(rows) > 0 and degree <= maxd:
-        dests = []
-        for r in rows:
-            src_filename = r[2]
-            dst_filename = r[3]
-            sz = os.path.getsize(src_filename)
-            json_dest = {}
-            json_dest['host'] = r[0]
-            json_dest['port'] = int(r[1])
-            json_dest['file'] = dst_filename
-            json_dest['id'] = r[4]
-            json_dest['block_size'] = 128*1024
-            json_dest['degree'] = degree
-            json_dest['length'] = sz
-            dests.append(json_dest)
-
-        final = {}
-        # for the sake of code resuse this will just be piped into an
-        # lt daemon processor.  /dev/null is used to supress a local write
-        final['file'] = "/dev/null"
-        final['host'] = "localhost"
-        final['port'] = 2893
-        final['block_size'] = 131072
-        final['degree'] = 1
-        final['id'] = str(uuid.uuid1())
-        final['destinations'] = dests
-
-        pylantorrent.log(logging.INFO, "request send %s" % (str(final)))
-        pylantorrent.log(logging.INFO, "sending em!")
-
-        client = LTClient(src_filename, final)
-        v = LTServer(client, client)
-        v.store_and_forward()
-
-        u = "update requests set state = ? where group_id = ?"
-        data = (1,group_id,)
-        c.execute(u, data)
-        state = 0
-        degree = degree + 1
-        if degree > maxd:
-            state = 2
-        rc = 0
-        es = client.get_incomplete()
-        bad_rid = []
-        for k in es:
-            rc = rc + 1
-            e = es[k]
-            if state != 2:
-                pylantorrent.log(logging.WARNING, "error trying to send %s" % (str(e)))
-            else:
-                pylantorrent.log(logging.ERROR, "error trying to send %s" % (str(e)))
-            rid = e['id']
-            bad_rid.append(rid)
-            u = "update requests set state = ?, message = ? where rid = ? and group_id = ?"
-            data = (state,str(e),rid,group_id,)
-            c.execute(u, data)
-        con.commit()
-    return rc
-
-
 def wait_until_sent(con, host, port, group_id):
     done = False
     while not done:
@@ -120,13 +46,11 @@ def main(argv=sys.argv[1:]):
 
     src_filename = argv[0]
     dst_filename = argv[1]
-    group_id = argv[2]
-    group_count = int(argv[3])
     # the user provides the rid.  that way we know they have it to look
     # things up later if needed
-    rid = argv[4]
+    rid = argv[2]
 
-    hostport = argv[5]
+    hostport = argv[3]
     ha = hostport.split(":")
     host = ha[0]
     if host == "":
@@ -142,8 +66,8 @@ def main(argv=sys.argv[1:]):
     now = datetime.datetime.now()
     con = sqlite3.connect(con_str)
 
-    i = "insert into requests(src_filename, dst_filename, group_id, group_count, hostname, port, rid, entry_time) values (?, ?, ?, ?, ?, ?, ?, ?)"
-    data = (src_filename, dst_filename, group_id, group_count, host, port, rid, now,)
+    i = "insert into requests(src_filename, dst_filename, hostname, port, rid, entry_time) values (?, ?, ?, ?, ?, ?)"
+    data = (src_filename, dst_filename, host, port, rid, now,)
 
     c = con.cursor()
     c.execute(i, data)
@@ -156,9 +80,6 @@ def main(argv=sys.argv[1:]):
     rs = c.fetchone()
     con.commit()
     cnt = int(rs[0])
-
-    if cnt == group_count:
-        do_it_live(con, group_id)
 
     (rc, message) = wait_until_sent(con, host, port, group_id)
     if rc == 0:
