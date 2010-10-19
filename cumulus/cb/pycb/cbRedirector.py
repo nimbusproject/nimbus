7c71961ae7ba74451327613997fd94bdaa4e0ba4
BuzzTroll <bresnaha@mcs.anl.gov>
2010-10-19 12:04:36 -1000

    merge conflicts

--- cumulus/cb/pycb/cbRedirector.py
+++ cumulus/cb/pycb/cbRedirector.py
@@ -1,9 +1,6 @@
 import os
 import sys
-<<<<<<< HEAD
 import random
-=======
->>>>>>> adding a redirector abstraction
 from pycb.cbException import cbException
 import pycb
 import stat
@@ -32,10 +29,6 @@ class cbRedirectorIface(object):
 class cbBasicRedirector(object):
 
     def __init__(self, parser):
-<<<<<<< HEAD
-=======
-        self.max = max_connections
->>>>>>> adding a redirector abstraction
         self.connection_count = 0
         self.host_file = parser.get("load_balanced", "hostfile")
         self.max = int(parser.get("load_balanced", "max"))
@@ -43,22 +36,14 @@ class cbBasicRedirector(object):
     def new_connection(self, request):
         h = None
         self.connection_count = self.connection_count + 1
-<<<<<<< HEAD
         if self.connection_count > self.max:
-=======
-        if g_connection_count >= self.max:
->>>>>>> adding a redirector abstraction
             h = self.get_next_host()
         return h
 
     def end_connection(self, request):
         self.connection_count = self.connection_count - 1
  
-<<<<<<< HEAD
     def get_next_host(self):
-=======
-    def get_next_host():
->>>>>>> adding a redirector abstraction
         try:
             hosts = []
             f = open(self.host_file, "r")
@@ -66,7 +51,6 @@ class cbBasicRedirector(object):
                 hosts.append(l.strip())
             f.close()
 
-<<<<<<< HEAD
             my_host = "%s:%d" % (pycb.config.hostname, pycb.config.port)
             for i in range(0, 5):
                 ndx = random.randint(0, len(hosts)-1)
@@ -78,17 +62,4 @@ class cbBasicRedirector(object):
         except Exception, ex:
             pycb.log(logging.ERROR, "get next host error %s" % (str(ex)))
             return None
-=======
-        my_host = "%s:%d" % (pycb.config.hostname, pycb.config.port)
-
-        for i in range(0, 10):
-            ndx = random.randint(0, len(hosts)-1)
-            h = hosts[ndx]
-            if h != my_host:
-                return h
-        return h
-    except Exception, ex:
-        log(logging.ERROR, "get next host error %s" % (str(ex)))
-        return None
->>>>>>> adding a redirector abstraction
  
