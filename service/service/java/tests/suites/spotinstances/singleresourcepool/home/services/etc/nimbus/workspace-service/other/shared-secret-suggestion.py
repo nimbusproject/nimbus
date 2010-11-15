205439373cc96f6d5cd81b0fc64d7fcbcfff4861
Tim Freeman <tim@timfreeman.org>
2010-11-14 22:43:02 -0600

    one suites.spotinstances change is to remove noresources distinction, this will be done programmatically

--- service/service/java/tests/suites/spotinstances/singleresourcepool/home/services/etc/nimbus/workspace-service/other/shared-secret-suggestion.py
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env python
-import string
-from random import Random
-okchars = string.letters + string.digits + "!@%^_&*+-"
-print ''.join( Random().sample(okchars, 25) )
--- service/service/java/tests/suites/spotinstances/singleresourcepool/home/services/share/nimbus/full-delete.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-BASEDIR_REL="`dirname $0`"
-BASEDIR=`cd $BASEDIR_REL; pwd`
-
-BUILDFILE=$BASEDIR/lib/db-mgmt.xml
-
-ant -q -f $BUILDFILE deleteAllPersistentData $*
