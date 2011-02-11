e7b479278f6da726ad936480f39f48c85497ac29
BuzzTroll <bresnaha@mcs.anl.gov>
2011-02-11 14:05:36 -0600

    adding starting and stopping of the right nimbus servers

--- docs/reload.py
+++ docs/reload.py
@@ -6,7 +6,17 @@ import tempfile
 old_nimbus = sys.argv[1]
 new_nimbus = sys.argv[2]
 
+cmd="%s/bin/nimbusctl stop" % (old_nimbus)
+os.system(cmd)
+cmd="%s/bin/nimbusctl stop" % (new_nimbus)
+os.system(cmd)
+cmd="%s/bin/nimbusctl start" % (old_nimbus)
+os.system(cmd)
 cmd="%s/bin/nimbus-nodes --list -b -D , -o %s" % (old_nimbus, tmp_file)
+os.system(cmd)
+cmd="%s/bin/nimbusctl stop" % (old_nimbus)
+os.system(cmd)
+cmd="%s/bin/nimbusctl start" % (new_nimbus)
 os.system(cmd)
 
 f = open(tmp_file, "r")
@@ -17,3 +27,6 @@ for line in f:
     new_cmd = "%s/bin/nimbus-nodes --add '%s' -p '%s' -m '%s' -n '%s'" % (new_nimbus, parts[0], parts[1], parts[2], parts[3])
 
     os.system(new_cmd)
+
+cmd="%s/bin/nimbusctl stop" % (new_nimbus)
+os.system(cmd)
