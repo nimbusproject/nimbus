5affef08f1d77f82348cb03b5bfd2912719fca34
John Bresnahan <buzztroll@gmail.com>
2011-05-27 12:48:24 -1000

    copy tests

--- tests/cp-prop-tests.sh
+++ tests/cp-prop-tests.sh
@@ -2,27 +2,38 @@
 
 tests="cc-prop2-test.py cc-unprop-md5test.py cc-propsamename-test.py cc-unprop-readwrite-test.py cc-prop-test.py cc-unprop-size-test.py cc-submit-common-unprop-size-test.py"
 
-old='<property name="repoScheme" value="scp" />'
-new='<property name="repoScheme" value="cp" />'
+old='scp'
+new='cp'
 
 conf="${NIMBUS_HOME}/services/etc/nimbus/workspace-service/other/authz-callout-ACTIVE.xml"
 
 bkup=`mktemp`
 cp $conf $bkup
 
+echo "setting up nimbus to use the cp propagation adapter"
+
+cmd="sed -i s^$old^$new^ $conf"
+echo $cmd
+$cmd
 ${NIMBUS_HOME}/bin/nimbusctl restart
 
-sed -i 's^$old^$new^' $conf
+function on_exit()
+{
+    echo "return to default propagation adapter"
+    mv $bkup $conf
+    ${NIMBUS_HOME}/bin/nimbusctl restart
+}
 
-echo "setting up nimbus to use the cp propagation adapter"
+trap on_exit EXIT
 
 for tst in $tests
 do
     echo "running $tst"
-    ./tst
-
+    ./$tst
+    if [ $? -ne 0 ]; then
+       echo "FAILED"
+       exit 1
+    fi
 done
 
-echo "return to default propagation adapter"
-cp $bkup $conf
-${NIMBUS_HOME}/bin/nimbusctl restart
+exit 0
