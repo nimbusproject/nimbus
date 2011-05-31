b525912b273a15ab9b919431af4c7b7ebd8fd9ed
Rob <rrusnak@uvic.ca>
2011-05-31 15:08:42 -0700

    testing commit

---
+++ service-api/java/source/src/org/nimbustools/api/services/admin/RemoteAdminToolsManagement.java
@@ -0,0 +1,11 @@
+package org.nimbustools.api.services.admin;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: rrusnak
+ * Date: 5/26/11
+ * Time: 11:06 AM
+ * To change this template use File | Settings | File Templates.
+ */
+public class RemoteAdminToolsManagement {
+}
---
+++ service/service/java/source/src/org/globus/workspace/remoting/admin/defaults/DefaultRemoteAdminToolsMgmt.java
@@ -0,0 +1,11 @@
+package org.globus.workspace.remoting.admin.defaults;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: rrusnak
+ * Date: 5/26/11
+ * Time: 11:13 AM
+ * To change this template use File | Settings | File Templates.
+ */
+public class DefaultRemoteAdminToolsMgmt {
+}
