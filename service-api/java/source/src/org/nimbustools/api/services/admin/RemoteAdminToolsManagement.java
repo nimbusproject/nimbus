9f5a2fa94da8b439d3e4d7734a0bde7e52f7ee91
Rob <rrusnak@uvic.ca>
2011-06-13 10:51:18 -0700

    First commit of nimbus-admin tools method. Run bin/nimbus-admin to use.

--- service-api/java/source/src/org/nimbustools/api/services/admin/RemoteAdminToolsManagement.java
+++ service-api/java/source/src/org/nimbustools/api/services/admin/RemoteAdminToolsManagement.java
@@ -1,11 +1,29 @@
 package org.nimbustools.api.services.admin;
 
+
+import java.rmi.Remote;
+import java.rmi.RemoteException;
+import java.util.Hashtable;
+
 /**
- * Created by IntelliJ IDEA.
+ * Copyright 1999-2010 University of Chicago
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy
+ * of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations
+ * under the License.
+ *
  * User: rrusnak
- * Date: 5/26/11
- * Time: 11:06 AM
- * To change this template use File | Settings | File Templates.
  */
-public class RemoteAdminToolsManagement {
+public interface RemoteAdminToolsManagement extends Remote {
+
+    public Hashtable getAllRunningVMs() throws RemoteException;
+
 }
