1a6f1dd1b9859007a1b6566e073d8b34622c967b
John Bresnahan <buzztroll@gmail.com>
2011-06-14 03:20:53 -1000

    more timeout adjustment

--- tests/cc-prop2-test.py
+++ tests/cc-prop2-test.py
@@ -17,7 +17,7 @@ try:
 except:
     pass
 cmd = "%s/bin/cloud-client.sh --transfer --sourcefile %s" % (cc_home, tst_image_src)
-(x, rc)=pexpect.run(cmd, withexitstatus=1, logfile=logfile)
+(x, rc)=pexpect.run(cmd, withexitstatus=1, logfile=logfile, timeout=to)
 
 cmd = "%s/bin/cloud-client.sh --run --name %s --hours .25" % (cc_home, tst_image_name)
 child = pexpect.spawn (cmd, timeout=to, maxread=20000, logfile=logfile)
@@ -39,7 +39,7 @@ if rc != 0:
     print "failed to save"
     sys.exit(1)
 cmd = "%s/bin/cloud-client.sh --list" % (cc_home)
-(x, rc)=pexpect.run(cmd, withexitstatus=1, logfile=logfile)
+(x, rc)=pexpect.run(cmd, withexitstatus=1, logfile=logfile, timeout=to)
 cmd = "%s/bin/cloud-client.sh --download --name %s --localfile %s" % (cc_home, newname, newname)
 print cmd
 (x, rc)=pexpect.run(cmd, withexitstatus=1, logfile=logfile, timeout=to)
