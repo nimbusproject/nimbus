5fc27ca364f2ce372fad838e922597a34fa1006d
David LaBissoniere <labisso@uchicago.edu>
2011-06-16 22:53:48 -0500

    Elastic SSH key persistence to sqlite

--- messaging/gt4.0-elastic/java/msgbridge/src/org/nimbustools/messaging/gt4_0_elastic/v2008_05_05/rm/defaults/DefaultElasticPersistence.java
+++ messaging/gt4.0-elastic/java/msgbridge/src/org/nimbustools/messaging/gt4_0_elastic/v2008_05_05/rm/defaults/DefaultElasticPersistence.java
@@ -18,6 +18,7 @@ package org.nimbustools.messaging.gt4_0_elastic.v2008_05_05.rm.defaults;
 import org.apache.commons.dbcp.BasicDataSource;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
+import org.nimbustools.messaging.gt4_0_elastic.v2008_05_05.security.SSHKey;
 import org.springframework.core.io.Resource;
 
 import javax.sql.DataSource;
@@ -26,6 +27,8 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
 
 public class DefaultElasticPersistence {
 
@@ -33,26 +36,41 @@ public class DefaultElasticPersistence {
             LogFactory.getLog(DefaultElasticPersistence.class.getName());
 
     //schema
-    static final String SCHEMA_CREATE_INSTANCES =
+    private static final String SCHEMA_CREATE_INSTANCES =
             "create table if not exists instances (" +
                     "    elastic_id varchar(10) unique not null," +
                     "    manager_id varchar(10) unique not null," +
                     "    reservation_id varchar(10) not null," +
                     "    sshkey varchar(128))";
-    static final String SCHEMA_CREATE_RESERVATIONS =
+    private static final String SCHEMA_CREATE_RESERVATIONS =
             "create table if not exists reservations (" +
                     "    reservation_id varchar(10) primary key," +
                     "    group_id varchar(10)," +
                     "    cosched_id varchar(10))";
-    static final String SCHEMA_CREATE_VERSION =
+    private static final String SCHEMA_CREATE_SSH_KEYPAIRS =
+            "create table if not exists ssh_keypairs (" +
+                    "    owner text not null," +
+                    "    keyname text not null," +
+                    "    pubkey text not null," +
+                    "    fingerprint text," +
+                    "    primary key (owner, keyname))";
+    private static final String SCHEMA_CREATE_VERSION =
             "create table if not exists schema_version (version int primary key)";
-    static final String SCHEMA_INSERT_VERSION =
+
+    // this acts as a 2-phase commit. if this record isn't present, the
+    // schema will be idempotently (re)created on the next service boot.
+    private static final String SCHEMA_INSERT_VERSION =
             "insert into schema_version (version) values(0)";
-    static final String[] SCHEMA_ALL =
+    private static final String[] SCHEMA_ALL =
             new String[]{SCHEMA_CREATE_INSTANCES, SCHEMA_CREATE_RESERVATIONS,
-                    SCHEMA_CREATE_VERSION, SCHEMA_INSERT_VERSION};
-    static final String GET_SCHEMA_VERSION =
+                    SCHEMA_CREATE_SSH_KEYPAIRS, SCHEMA_CREATE_VERSION,
+                    SCHEMA_INSERT_VERSION};
+
+    // schema check query
+    private static final String GET_SCHEMA_VERSION =
             "select version from schema_version limit 1";
+
+
     static final String INSERT_INSTANCE = "insert into instances " +
             "(elastic_id, manager_id, reservation_id, sshkey) values(?,?,?,?)";
     static final String GET_MANAGER_FROM_ELASTIC_INSTANCE =
@@ -76,6 +94,18 @@ public class DefaultElasticPersistence {
     // a little silly
     static final String GET_RESERVATION =
             "select reservation_id from reservations where reservation_id = ?";
+
+    static final String GET_SSH_KEY = "select owner, keyname, pubkey, fingerprint " +
+            "from ssh_keypairs where owner = ? and keyname = ?";
+    static final String GET_SSH_KEYS_BY_OWNER = "select owner, keyname, pubkey, " +
+            "fingerprint from ssh_keypairs where owner = ?";
+    static final String INSERT_SSH_KEY = "insert into ssh_keypairs " +
+            "(owner, keyname, pubkey, fingerprint) values(?,?,?,?)";
+    static final String UPDATE_SSH_KEY = "updatessh_keypairs " +
+                "set pubkey = ?, fingerprint = ? where owner = ? and keyname = ?";
+    static final String DELETE_SSH_KEY = "delete from ssh_keypairs " +
+            "where owner = ? and keyname = ?";
+
 
     private final DataSource dataSource;
 
@@ -272,6 +302,182 @@ public class DefaultElasticPersistence {
             }
         }
     }
+
+    public List<SSHKey> getSSHKeys(String owner) throws Exception {
+        if (owner == null) {
+            throw new IllegalArgumentException("owner may not be null");
+        }
+        return this.getSSHKeys(owner, null);
+    }
+
+    public SSHKey getSSHKey(String owner, String keyname) throws Exception {
+        if (owner == null) {
+            throw new IllegalArgumentException("owner may not be null");
+        }
+        if (keyname == null) {
+            throw new IllegalArgumentException("keyname may not be null");
+        }
+        final List<SSHKey> keys = getSSHKeys(owner, keyname);
+        if (keys.isEmpty()) {
+            return null;
+        }
+        return keys.get(0);
+    }
+
+    public void putSSHKey(SSHKey key) throws Exception {
+        if (key == null) {
+            throw new IllegalArgumentException("key may not be null");
+        }
+        Connection c = null;
+        PreparedStatement pstmt = null;
+
+        try {
+            c = dataSource.getConnection();
+            pstmt = c.prepareStatement(INSERT_SSH_KEY);
+            pstmt.setString(1, key.getOwnerID());
+            pstmt.setString(2, key.getKeyName());
+            pstmt.setString(3, key.getPubKeyValue());
+            pstmt.setString(4, key.getFingerprint());
+
+            int rc = pstmt.executeUpdate();
+            if (rc != 1) {
+                throw new Exception("did not insert the row properly");
+            }
+        } catch (SQLException e) {
+            logger.error("", e);
+            throw e;
+        } finally {
+            try {
+                if (pstmt != null) {
+                    pstmt.close();
+                }
+                if (c != null) {
+                    returnConnection(c);
+                }
+            } catch (SQLException sql) {
+                logger.error("SQLException in finally cleanup", sql);
+            }
+        }
+    }
+
+    public boolean updateSSHKey(SSHKey key) throws Exception {
+        if (key == null) {
+            throw new IllegalArgumentException("key may not be null");
+        }
+        Connection c = null;
+        PreparedStatement pstmt = null;
+
+        try {
+            c = dataSource.getConnection();
+            pstmt = c.prepareStatement(UPDATE_SSH_KEY);
+            pstmt.setString(1, key.getPubKeyValue());
+            pstmt.setString(2, key.getFingerprint());
+            pstmt.setString(3, key.getOwnerID());
+            pstmt.setString(4, key.getKeyName());
+
+            int rc = pstmt.executeUpdate();
+            return rc != 0;
+
+        } catch (SQLException e) {
+            logger.error("", e);
+            throw e;
+        } finally {
+            try {
+                if (pstmt != null) {
+                    pstmt.close();
+                }
+                if (c != null) {
+                    returnConnection(c);
+                }
+            } catch (SQLException sql) {
+                logger.error("SQLException in finally cleanup", sql);
+            }
+        }
+    }
+
+    public boolean deleteSSHKey(String owner, String keyname) throws Exception {
+        if (owner == null) {
+            throw new IllegalArgumentException("owner may not be null");
+        }
+        if (keyname == null) {
+            throw new IllegalArgumentException("keyname may not be null");
+        }
+        Connection c = null;
+        PreparedStatement pstmt = null;
+
+        try {
+            c = dataSource.getConnection();
+            pstmt = c.prepareStatement(DELETE_SSH_KEY);
+            pstmt.setString(1, owner);
+            pstmt.setString(2, keyname);
+
+            int rc = pstmt.executeUpdate();
+            return rc != 0;
+
+        } catch (SQLException e) {
+            logger.error("", e);
+            throw e;
+        } finally {
+            try {
+                if (pstmt != null) {
+                    pstmt.close();
+                }
+                if (c != null) {
+                    returnConnection(c);
+                }
+            } catch (SQLException sql) {
+                logger.error("SQLException in finally cleanup", sql);
+            }
+        }
+    }
+
+    private List<SSHKey> getSSHKeys(String owner, String keyname) throws SQLException {
+
+        Connection c = null;
+        PreparedStatement pstmt = null;
+        ResultSet rs;
+
+        try {
+            c = dataSource.getConnection();
+
+            if (keyname == null) {
+                pstmt = c.prepareStatement(GET_SSH_KEYS_BY_OWNER);
+                pstmt.setString(1, owner);
+             } else {
+                pstmt = c.prepareStatement(GET_SSH_KEY);
+                pstmt.setString(1, owner);
+                pstmt.setString(2, keyname);
+            }
+            rs = pstmt.executeQuery();
+
+            List<SSHKey> keys = new ArrayList<SSHKey>(4);
+            while (rs.next()) {
+                SSHKey key = new SSHKey(
+                        rs.getString("owner"),
+                        rs.getString("keyname"),
+                        rs.getString("pubkey"),
+                        rs.getString("fingerprint"));
+                keys.add(key);
+            }
+            return keys;
+        } catch (SQLException e) {
+            logger.error("", e);
+            throw e;
+        } finally {
+            try {
+                if (pstmt != null) {
+                    pstmt.close();
+                }
+                if (c != null) {
+                    returnConnection(c);
+                }
+            } catch (SQLException sql) {
+                logger.error("SQLException in finally cleanup", sql);
+            }
+        }
+    }
+
+
 
     private void returnConnection(Connection connection) {
         if (connection != null) {
