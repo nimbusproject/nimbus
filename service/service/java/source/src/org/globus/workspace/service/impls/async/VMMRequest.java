9b096b86127ddecb56e92e2e761cf7535d31420a
Carla Souza <crlnh.a@gmail.com>
2011-07-27 08:16:03 -0300

    Method to set the context for a vmmrequest

--- service/service/java/source/src/org/globus/workspace/service/impls/async/VMMRequest.java
+++ service/service/java/source/src/org/globus/workspace/service/impls/async/VMMRequest.java
@@ -2,15 +2,11 @@ package org.globus.workspace.service.impls.async;
 
 import org.globus.workspace.WorkspaceException;
 
-/**
- * Created by IntelliJ IDEA.
- * User: carla
- * Date: 17/07/11
- * Time: 10:37
- * To change this template use File | Settings | File Templates.
- */
 public interface VMMRequest {
 
     public String execute() throws WorkspaceException;
+
+    public void setRequestContext(VMMRequestContext requestContext);
+
 
 }
