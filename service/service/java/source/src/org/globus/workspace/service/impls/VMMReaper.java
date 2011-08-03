5904f1d35afc47e5c63b071886432b4514b2d890
Carla Souza <crlnh.a@gmail.com>
2011-08-03 10:15:49 -0300

    Documentation; Minor fix

--- service/service/java/source/src/org/globus/workspace/service/impls/VMMReaper.java
+++ service/service/java/source/src/org/globus/workspace/service/impls/VMMReaper.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 1999-2008 University of Chicago
+ * Copyright 1999-2011 University of Chicago
  *
  * Licensed under the Apache License, Version 2.0 (the "License"); you may not
  * use this file except in compliance with the License. You may obtain a copy
